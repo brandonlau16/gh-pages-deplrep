19a20a4d809b37bd46bc2c6019531d04a0098cae
brandonlau16 <brandon_lee162@hotmail.com>
2021-06-23 20:58:54 -0500

    hello world

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React helooooo!!!
+          Learn React hellooooo world!!!
         </a>
       </header>
     </div>
